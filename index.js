5b55d6e1bf7509430ec82b51ab6e712250c48724
SenPai-13 <arjunpai.1031@gmail.com>
2025-02-25 12:59:04 +0530

    a

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ app.get('/cart/delete', (req, res) => {
 
 // Endpoint 4: Read Items in the Cart
 app.get('/cart', (req, res) => {
-  res.json(cart);
+  res.json([]);
 });
 
 // Endpoint 5: Calculate Total Quantity of Items in the Cart
